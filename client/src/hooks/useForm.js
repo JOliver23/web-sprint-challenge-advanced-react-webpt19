4506d84fb48198ddcdba1176fdf3a1a57a883cde
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-09-11 18:25:47 -0700

    built custom hook for form submission; returning state variable and functions

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,24 @@
 // write your custom hook here to control your checkout form
+import { useState } from "react";
+
+export const useForm = (initialState, key) => {
+  const [showSuccessMessage, setShowSuccessMessage] = useState(key);
+  const [values, setValues] = useState(
+    initialState,
+    key
+  );
+
+  const handleChanges = e => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setShowSuccessMessage(true);
+  };
+
+  return [values, showSuccessMessage, handleChanges, handleSubmit];
+};
